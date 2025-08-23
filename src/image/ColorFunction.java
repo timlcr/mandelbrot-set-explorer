e268f854f1cf0cc1342932dd28fc9582f7f97494
Tim Le Cheminant-Reid <timlcr03@gmail.com>
2025-08-23 14:06:46 +1200

    Added comments in ColorFunction.java and MandelbrotImage.java

--- src/image/ColorFunction.java
+++ src/image/ColorFunction.java
@@ -5,6 +5,10 @@ import algorithm.RepresentationValue;
 import java.awt.Color;
 import java.util.function.BiFunction;
 
+/**
+ * Used to determine the colour of each pixel in a <code>MandelbrotImage</code> by being
+ * evaluated on each RepresentationValue in the array stored in a MandelbrotImage.
+ */
 public interface ColorFunction extends BiFunction<RepresentationValue, ColorFunctionParameters, Color> {
 
     ColorFunction BLACK_AND_WHITE = (val, params) ->
--- src/image/MandelbrotImage.java
+++ src/image/MandelbrotImage.java
@@ -5,6 +5,11 @@ import util.Complex;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * A visual representation of the Mandelbrot Set stored in a BufferedImage object.
+ * Each pixel corresponds to a point in the complex plane. The colour of each pixel is determined by
+ * information gathered after running an algorithm on the corresponding complex point.
+ */
 public class MandelbrotImage extends BufferedImage {
 
     private final RepresentationValue[][] array;
@@ -15,7 +20,15 @@ public class MandelbrotImage extends BufferedImage {
 
     private ColorFunction colorFunction;
 
-
+    /**
+     * Constructor
+     * @param width the width of the image in pixels
+     * @param height the height of the image in pixels
+     * @param center the complex point the image is centered on
+     * @param zoom the level of zoom. This is equal to the imaginary range of the area
+     *             of the complex plane covered by the image.
+     * @param maxN the maximum number of iterations allowed when running the algorithm on each point
+     */
     public MandelbrotImage(int width, int height, Complex center, double zoom, int maxN) {
         super(width, height, BufferedImage.TYPE_INT_RGB);
         array = new RepresentationValue[width][height];
