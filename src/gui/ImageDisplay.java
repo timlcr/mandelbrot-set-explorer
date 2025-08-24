43e9a6fc7e2db4b58826abf852ed00baeaaac71f
Tim Le Cheminant-Reid <timlcr03@gmail.com>
2025-08-24 16:20:12 +1200

    Created GUI class ImageDisplay which displays MandelbrotImages with the maximum size maintaining the image aspect ratio. Includes some buttons for testing

---
+++ src/gui/ImageDisplay.java
@@ -0,0 +1,103 @@
+package gui;
+
+import image.ColorFunction;
+import image.ColorFunctionParameters;
+import image.MandelbrotImage;
+import util.Complex;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class ImageDisplay extends JPanel {
+
+    private MandelbrotImage image;
+
+    private final JPanel imageArea = new JPanel(new GridBagLayout());
+    private final JPanel imagePanel = imagePanel();
+
+    public ImageDisplay() {
+        super(new BorderLayout());
+        add(imageArea, BorderLayout.CENTER);
+        imageArea.add(imagePanel, new GridBagConstraints());
+        imageArea.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) { layoutImageArea(); }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(image1button());
+        buttonPanel.add(image2button());
+        buttonPanel.add(image3button());
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public void setImage(MandelbrotImage image) {
+        this.image = image;
+        layoutImageArea();
+    }
+
+    private JPanel imagePanel() {
+        JPanel panel =  new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (image == null) return;
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        panel.setBorder(new LineBorder(Color.BLACK));
+        return panel;
+    }
+
+    private void layoutImageArea() {
+        if (image == null) return;
+        double imageAspectRatio = image.getWidth() / (double) image.getHeight();
+        double areaAspectRatio = imageArea.getWidth() / (double) imageArea.getHeight();
+        if (areaAspectRatio > imageAspectRatio) {
+            int imageWidth = (int) (imageArea.getHeight() * imageAspectRatio);
+            imagePanel.setPreferredSize(new Dimension(imageWidth, imageArea.getHeight()));
+        } else {
+            int imageHeight = (int) (imageArea.getWidth() / imageAspectRatio);
+            imagePanel.setPreferredSize(new Dimension(imageArea.getWidth(), imageHeight));
+        }
+        imagePanel.revalidate();
+        imagePanel.repaint();
+    }
+
+    MandelbrotImage image1 = MandelbrotImage.of(500, 500, Complex.DEFAULT_CENTER, 2.5, 100,
+            ColorFunction.BLACK_AND_WHITE, ColorFunctionParameters.defaultParameters());
+
+    MandelbrotImage image2 = MandelbrotImage.of(750, 500, Complex.DEFAULT_CENTER, 2.5, 100,
+            ColorFunction.BLACK_AND_WHITE, ColorFunctionParameters.defaultParameters());
+
+    MandelbrotImage image3 = MandelbrotImage.of(500, 1000, Complex.DEFAULT_CENTER, 2.5, 100,
+            ColorFunction.BLACK_AND_WHITE, ColorFunctionParameters.defaultParameters());
+
+    private JButton image1button() {
+        JButton button = new JButton("Image 1");
+        button.addActionListener(e -> setImage(image1));
+        return button;
+    }
+
+    private JButton image2button() {
+        JButton button = new JButton("Image 2");
+        button.addActionListener(e -> setImage(image2));
+        return button;
+    }
+
+    private JButton image3button() {
+        JButton button = new JButton("Image 3");
+        button.addActionListener(e -> setImage(image3));
+        return button;
+    }
+
+}
