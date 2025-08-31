17421860cd138cca8362762b3be3c44874d0ea55
Tim Le Cheminant-Reid <timlcr03@gmail.com>
2025-08-31 15:44:17 +1200

    Added button to go back to the previous zoom in Explorer.

--- src/gui/Explorer.java
+++ src/gui/Explorer.java
@@ -6,6 +6,7 @@ import util.Complex;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.util.Stack;
 
 /**
  * A gui component which allows the user to explore the Mandelbrot Set by selecting the area
@@ -13,7 +14,9 @@ import java.awt.BorderLayout;
  */
 public class Explorer extends JPanel {
 
-    private final ExplorerDisplay display = new ExplorerDisplay();
+    private final Stack<MandelbrotImage> imageCache = new Stack<>();
+
+    private final ExplorerDisplay display = new ExplorerDisplay(imageCache);
     private final ColorControls colorControls = new ColorControls(this::handleRecolor, null);
 
     private final Runnable sendCoords;
@@ -42,11 +45,21 @@ public class Explorer extends JPanel {
     }
 
     private void reset() {
+        imageCache.clear();
         double defaultZoom = 3;
         MandelbrotImage base = new MandelbrotImage(500, 500, Complex.DEFAULT_CENTER, defaultZoom, 1000,
                 colorControls.getColorFunction(), colorControls.getColorFuncParams(defaultZoom));
         display.setImage(base);
         display.repaint();
+    }
+
+    private void showPreviousImage() {
+        if (imageCache.isEmpty()) return;
+        MandelbrotImage image = imageCache.pop();
+        image.setColorFunction(colorControls.getColorFunction());
+        image.setColorFuncParams(colorControls.getColorFuncParams(image.zoom));
+        image.colorImage();
+        display.setImage(image);
     }
 
     private void handleRecolor() {
@@ -62,12 +75,15 @@ public class Explorer extends JPanel {
     private JPanel buttonPanel() {
         JPanel buttonPanel = new JPanel();
         JButton resetButton = new JButton("Reset");
+        JButton previousButton = new JButton("Show previous zoom");
         JButton colorButton = new JButton("Color");
         JButton sendCoordsButton = new JButton("Send coordinates to Studio");
         resetButton.addActionListener(e -> reset());
+        previousButton.addActionListener(e -> showPreviousImage());
         colorButton.addActionListener(e -> colorControls.setVisible(true));
         sendCoordsButton.addActionListener(e -> sendCoords.run());
         buttonPanel.add(resetButton);
+        buttonPanel.add(previousButton);
         buttonPanel.add(colorButton);
         buttonPanel.add(sendCoordsButton);
         return buttonPanel;
--- src/gui/ExplorerDisplay.java
+++ src/gui/ExplorerDisplay.java
@@ -1,5 +1,6 @@
 package gui;
 
+import image.MandelbrotImage;
 import image.SuccessiveMandelbrotImage;
 import util.Complex;
 
@@ -9,11 +10,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Stack;
 
 /**
  * Image display area for the explorer gui.
  */
 public class ExplorerDisplay extends ImageDisplay {
+
+    private final Stack<MandelbrotImage> imageCache;
+
+    public ExplorerDisplay(Stack<MandelbrotImage> imageCache) {
+        this.imageCache = imageCache;
+    }
 
     @Override
     protected ImagePanel imagePanel() {
@@ -55,6 +63,7 @@ public class ExplorerDisplay extends ImageDisplay {
          * @param zoom the zoom level of the next image
          */
         private void nextImage(Complex center, double zoom) {
+            imageCache.push(image);
             int maxN = 1000;
             if (zoom < 2e-4) maxN = 2000;
             SuccessiveMandelbrotImage nextImage = new SuccessiveMandelbrotImage(
