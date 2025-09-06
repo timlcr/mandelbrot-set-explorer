b9c84f74fa2624199da43e22b61291dfb4e9f912
Tim Le Cheminant-Reid <timlcr03@gmail.com>
2025-09-06 14:05:22 +1200

    Deleted class ExplorerDisplay. Moved method nextImage into Explorer

--- src/gui/Explorer.java
+++ src/gui/Explorer.java
@@ -1,12 +1,15 @@
 package gui;
 
 import image.MandelbrotImage;
+import image.SuccessiveMandelbrotImage;
 import util.Complex;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
@@ -23,7 +26,7 @@ public class Explorer extends JPanel {
 
     private final Stack<MandelbrotImage> imageCache = new Stack<>();
 
-    private final ExplorerDisplay display = new ExplorerDisplay(imageCache);
+    private final ImageDisplay display = new ImageDisplay();
     private final ColorControls colorControls = new ColorControls(
             this::handleRecolor, "Explorer colour controls", COLOR, null
     );
@@ -36,6 +39,7 @@ public class Explorer extends JPanel {
     public Explorer(Runnable sendCoords) {
         super(new BorderLayout());
         this.sendCoords = sendCoords;
+        new SubzoomSelector(display.imagePanel, this::nextImage);
 
         display.setBorder(GUI.imageDisplayBorder());
         add(display, BorderLayout.CENTER);
@@ -51,6 +55,35 @@ public class Explorer extends JPanel {
 
     public void colorControlsVisible(boolean visible) {
         colorControls.setVisible(visible);
+    }
+
+    /**
+     * Creates the next image and displays it. The image used is a SuccessiveMandelbrotImage,
+     * which uses successive refinement to boost responsiveness and efficiency.
+     * @param center the complex point the next image is centered on
+     * @param zoom the zoom level of the next image
+     */
+    private void nextImage(Complex center, double zoom) {
+        MandelbrotImage image = display.image;
+        imageCache.push(image);
+        int maxN = 1000;
+        if (zoom < 2e-4) maxN = 2000;
+        SuccessiveMandelbrotImage nextImage = new SuccessiveMandelbrotImage(
+                center, zoom, maxN, image.colorFunctionType(), image.colorFuncParams()
+        );
+        nextImage.setFilamentSize(image.getFilamentSize());
+        display.setImage(nextImage);
+        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+            @Override
+            protected Void doInBackground() {
+                for (int i = 0; i < 5; i++) {
+                    nextImage.refine();
+                    SwingUtilities.invokeLater(() -> repaint());
+                }
+                return null;
+            }
+        };
+        worker.execute();
     }
 
     private void reset() {
